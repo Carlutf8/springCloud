8545706f34ffe98fc4b2dbc7bc96ff022baf0826
hechuanbin <824681014@qq.com>
2018-05-08 23:01:11 +0800

    shiro-demo

---
+++ shiro-server/src/main/java/com/example/demo/ShiroServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroServerApplication.class, args);
+	}
+}
